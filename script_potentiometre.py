c6912afef32e34de8973efd583182961b11c7e4c
JuliaCaudron <167758646+JuliaCaudron@users.noreply.github.com>
2025-02-18 17:03:10 +0100

    ajout_MCP3008+potar_script

---
+++ script_potentiometre.py
@@ -0,0 +1,5 @@
+from MCP3008 import MCP3008
+
+adc = MCP3008()
+value = adc.read( channel = 0 ) # Vous pouvez bien entendu adapter le canal à lire
+print("Tension appliquée : %.2f" % (valeur / 1023.0 * 3.3) )
--- script_test_1_moteur.py
+++ script_test_1_moteur.py
@@ -18,7 +18,7 @@ try:
     while True:
         # Test moteur 1 (GV) :
         GPIO.output(sens_GV, GPIO.HIGH) # on force le sens de rotation du moteur
-        # HIGH = tension négative aux bornes du moteur (LED verte)
+        # HIGH = tension négative aux bornes du moteur (LED verte) -> rotation sens anti-horaire vu de dessus -> voile à babord
         PWM_GV.ChangeDutyCycle(20) # on fait tourner le moteur à 30% de sa vitesse
 
         sleep(1) # maintient la vitesse pendant 5 secondes
@@ -28,7 +28,7 @@ try:
         sleep(2) # s'arrête pendant 2s
 
         GPIO.output(sens_GV, GPIO.LOW) # on inverse le sens de rotation du moteur
-        # LOW = tension positive aux bornes du moteur (LED rouge)
+        # LOW = tension positive aux bornes du moteur (LED rouge) -> rotation sens horaire vu de dessus -> voile à tribord
         PWM_GV.ChangeDutyCycle(20)
 
         sleep(1) # les moteurs tournent dans l'autre sens pendant 5s
