cb4f02c45f10070625bea6fb7495deccb6e29af0
JuliaCaudron <167758646+JuliaCaudron@users.noreply.github.com>
2025-02-24 16:24:12 +0100

    Update script_potentiometre.py
    erreur d'orthographe

--- script_potentiometre.py
+++ script_potentiometre.py
@@ -3,4 +3,4 @@ from MCP3008 import MCP3008
 
 adc = MCP3008()
 value = adc.read( channel = 0 ) # Vous pouvez bien entendu adapter le canal à lire
-print("Tension appliquée : %.2f" % (valeur / 1023.0 * 3.3) )
+print("Tension appliquée : %.2f" % (value / 1023.0 * 3.3) )
