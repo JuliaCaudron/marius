e9bef22572c6183e2cfd82dc72da80db20745c3d
JuliaCaudron <167758646+JuliaCaudron@users.noreply.github.com>
2025-02-24 16:26:38 +0100

    Update script_potentiometre.py
    ajout boucle while

--- script_potentiometre.py
+++ script_potentiometre.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from MCP3008 import MCP3008
 
-adc = MCP3008()
-value = adc.read( channel = 0 ) # Vous pouvez bien entendu adapter le canal à lire
-print("Tension appliquée : %.2f" % (value / 1023.0 * 3.3) )
+try:
+    while True:
+        adc = MCP3008()
+        value = adc.read( channel = 0 ) # Vous pouvez bien entendu adapter le canal à lire
+        print("Tension appliquée : %.2f" % (value / 1023.0 * 3.3) )
+
+except KeyboardInterrupt:
+    print("Arrêt du programme")
