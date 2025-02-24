926ac334837ff531a24451bfa8c306f9b7ea3e9f
JuliaCaudron <167758646+JuliaCaudron@users.noreply.github.com>
2025-02-24 16:29:09 +0100

    Update script_potentiometre.py

--- script_potentiometre.py
+++ script_potentiometre.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from MCP3008 import MCP3008
+from time import sleep
 
 try:
     while True:
         adc = MCP3008()
         value = adc.read( channel = 0 ) # Vous pouvez bien entendu adapter le canal à lire
         print("Tension appliquée : %.2f" % (value / 1023.0 * 3.3) )
+        sleep(1)
 
 except KeyboardInterrupt:
     print("Arrêt du programme")
