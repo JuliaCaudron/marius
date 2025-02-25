3526f6e7a9c7f67acc2fca4a33770747f771959e
JuliaCaudron <167758646+JuliaCaudron@users.noreply.github.com>
2025-02-25 14:38:07 +0100

    Update script_moteurs_2.py

--- script_moteurs_2.py
+++ script_moteurs_2.py
@@ -27,7 +27,7 @@ try:
         GPIO.output(sens_GV, GPIO.HIGH) # on force le sens de rotation du moteur
         GPIO.output(sens_saf, GPIO.HIGH)
         PWM_GV.ChangeDutyCycle(20) # on fait tourner le moteur à 30% de sa vitesse
-        #PWM_saf.ChangeDutyCycle(10)
+        PWM_saf.ChangeDutyCycle(10)
 
         sleep(1) # maintient la vitesse pendant 5 secondes
 
@@ -39,7 +39,7 @@ try:
         GPIO.output(sens_GV, GPIO.LOW) # on inverse le sens de rotation du moteur
         GPIO.output(sens_saf, GPIO.LOW)
         PWM_GV.ChangeDutyCycle(20)
-        #PWM_saf.ChangeDutyCycle(10)
+        PWM_saf.ChangeDutyCycle(10)
 
         sleep(1) # les moteurs tournent dans l'autre sens pendant 5s
 
